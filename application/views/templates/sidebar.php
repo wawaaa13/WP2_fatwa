3e05821b3785b0a13a6d609339b73dbf8fd29af6
wawaaa13 <fatwahutrian1330@gmail.com>
2021-11-18 16:40:16 +0700

    pertemuan 10

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -23,7 +23,12 @@ Booking</div>
                 <div class="sidebar-heading"> 
                     Master Data 
                 </div> 
- 
+                <li class="nav-item">
+        <a class="nav-link pb-0" href="<?=
+                                        base_url('buku'); ?>">
+            <i class="fa fa-fw fa book"></i>
+            <span>Kategori Buku</span></a>
+    </li>
                     <!-- Nav Item - Dashboard --> 
                     <li class="nav-item active"> 
                         <!-- Nav Item - Dashboard --> 
